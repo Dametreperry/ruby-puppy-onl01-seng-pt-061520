bf5cf7349d793ff7ad1213c9b85a1e65e1df82fc
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-14 03:59:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,26 @@
 class Dog 
-  
+  attr_accessor :name 
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end
-  
-  def self.all
+   @name = name
+   save
+  end 
+
+  def self.all 
     @@all
-  end
+  end 
   
   def self.print_all
-    @@all do |name|
-      puts "#{name}"
-    end
-  end
-end
-end
+    @@all.each.collect {|dog| puts dog.name} 
+    binding.pry
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.clear_all
+   @@all.clear
+  end 
+end 
